d25866d05f63cbe4aa5edf1d5bf601b632b4b80a
tonimolk <149098213+tonimolk@users.noreply.github.com>
2024-11-25 21:25:29 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -3,15 +3,39 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { PaperProvider, MD3LightTheme, adaptNavigationTheme } from 'react-native-paper';
 const Stack = createStackNavigator();
 const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function MusicScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Music"
+        onPress={() => navigation.navigate('Music')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <PaperProvider theme={MD3LightTheme}>
       <NavigationContainer theme={LightTheme}>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Music" component={MusicScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
-  );
+  ); 
 }
