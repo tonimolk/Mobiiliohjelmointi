b846f11ec5184bb2c6434f4812661b45c1cd65d2
toni M <bha576@myy.haaga-helia.fi>
2024-11-27 19:05:27 +0200

    check

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { PaperProvider, MD3LightTheme, adaptNavigationTheme } from 'react-native-paper';
+const Stack = createStackNavigator();
+const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MusicScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Music"
+        onPress={() => navigation.navigate('Music')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <PaperProvider theme={MD3LightTheme}>
+      <NavigationContainer theme={LightTheme}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Music" component={MusicScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  ); 
+}
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
+
+const theme = {
+    ...DefaultTheme,
+        colors: {
+    primary: "rgb(186, 26, 32)",
+    onPrimary: "rgb(255, 255, 255)",
+    secondary: "rgb(121, 89, 0)",
+    onSecondary: "rgb(255, 255, 255)",
+                }
+            }
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
